4ee8624ba7134a6b86eaccc8ab26ed29e4f02686
ShahilJosh <159491127+ShahilJosh@users.noreply.github.com>
2024-03-18 13:02:48 +0530

    Add files via upload

---
+++ readcv.py
@@ -0,0 +1,70 @@
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFPageInterpreter
+from pdfminer.pdfinterp import PDFResourceManager
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+import io
+
+def extract_text_from_pdf(pdf_path):
+    text = ''  # initialize text variable
+    with open(pdf_path, 'rb') as fh:
+        # iterate over all pages of PDF document
+        for page in PDFPage.get_pages(fh, caching=True, check_extractable=True):
+            # creating a resoure manager
+            resource_manager = PDFResourceManager()
+            
+            # create a file handle
+            fake_file_handle = io.StringIO()
+            
+            # creating a text converter object
+            converter = TextConverter(
+                                resource_manager, 
+                                fake_file_handle, 
+                                codec='utf-8', 
+                                laparams=LAParams()
+                        )
+
+            # creating a page interpreter
+            page_interpreter = PDFPageInterpreter(
+                                resource_manager, 
+                                converter
+                            )
+
+            # process current page
+            page_interpreter.process_page(page)
+            
+            # extract text
+            text += ' ' + fake_file_handle.getvalue()
+            
+            # close open handles
+            converter.close()
+            fake_file_handle.close()
+    
+    return text
+
+# calling above function and extracting text
+text = extract_text_from_pdf(r"C:\Users\joshu\Downloads\Shahil Joshua stephen.pdf")
+#to show obtained txt add print(text)
+
+import spacy
+from spacy.matcher import Matcher
+
+# pre-trained model
+nlp = spacy.load('en_core_web_sm')
+
+# initialize matcher with a vocab
+matcher = Matcher(nlp.vocab)
+
+def extract_name(resume_text):
+    nlp_text = nlp(resume_text)
+    
+    # First name and Last name are always Proper Nouns
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}]
+    
+    matcher.add('NAME', None, *pattern)
+    
+    matches = matcher(nlp_text)
+    
+    for match_id, start, end in matches:
+        span = nlp_text[start:end]
+        return span.text
