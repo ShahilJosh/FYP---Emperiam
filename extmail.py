2f0c4206f747cb2ce048e51948f02f57d01c34cc
ShahilJosh <159491127+ShahilJosh@users.noreply.github.com>
2024-03-21 00:17:06 +0530

    Delete extmail.py

--- extmail.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def extract_email(email):
-    email = re.findall("([^@|\s]+@[^@]+\.[^@|\s]+)", email)
-    if email:
-        try:
-            return email[0].split()[0].strip(';')
-        except IndexError:
-            return None
